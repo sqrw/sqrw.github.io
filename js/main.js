1cdab0b0d5fb22ec067c7f9b3ba435d7d4d763fa
StarCitizenTracker <cixoluhofe@vpsorg.pro>
2017-05-24 05:29:57 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,80 @@
+//scrolling to anchor
+(function(document, history, location) {
+  var HISTORY_SUPPORT = !!(history && history.pushState);
+
+  var anchorScrolls = {
+    ANCHOR_REGEX: /^#[^ ]+$/,
+    OFFSET_HEIGHT_PX: 50,
+
+    /**
+     * Establish events, and fix initial scroll position if a hash is provided.
+     */
+    init: function() {
+      this.scrollToCurrent();
+      $(window).on('hashchange', $.proxy(this, 'scrollToCurrent'));
+      $('body').on('click', 'a', $.proxy(this, 'delegateAnchors'));
+    },
+
+    /**
+     * Return the offset amount to deduct from the normal scroll position.
+     * Modify as appropriate to allow for dynamic calculations
+     */
+    getFixedOffset: function() {
+      return this.OFFSET_HEIGHT_PX;
+    },
+
+    /**
+     * If the provided href is an anchor which resolves to an element on the
+     * page, scroll to it.
+     * @param  {String} href
+     * @return {Boolean} - Was the href an anchor.
+     */
+    scrollIfAnchor: function(href, pushToHistory) {
+      var match, anchorOffset;
+
+      if(!this.ANCHOR_REGEX.test(href)) {
+        return false;
+      }
+
+      match = document.getElementById(href.slice(1));
+
+      if(match) {
+        anchorOffset = $(match).offset().top - this.getFixedOffset();
+        $('html, body').animate({ scrollTop: anchorOffset});
+
+        // Add the state to history as-per normal anchor links
+        if(HISTORY_SUPPORT && pushToHistory) {
+          history.pushState({}, document.title, location.pathname + href);
+        }
+      }
+
+      return !!match;
+    },
+    
+    /**
+     * Attempt to scroll to the current location's hash.
+     */
+    scrollToCurrent: function(e) { 
+      if(this.scrollIfAnchor(window.location.hash) && e) {
+      	e.preventDefault();
+      }
+    },
+
+    /**
+     * If the click event's target was an anchor, fix the scroll position.
+     */
+    delegateAnchors: function(e) {
+      var elem = e.target;
+
+      if(this.scrollIfAnchor(elem.getAttribute('href'), true)) {
+        e.preventDefault();
+      }
+    }
+  };
+$(document).ready($.proxy(anchorScrolls, 'init'));
+})(window.document, window.history, window.location);
+
+
 (function($, List, _, moment) {
 
   // List.js classes to use for search elements
@@ -148,9 +225,8 @@ function Build_and_fill_Chart(para_Type) {
         { "Not_implemented": 0, "Broken": 0, "Completed": 0, "In_alpha": 0, "Compromised": 0, "date": "2012-10-01T07:00:00.000Z" },
         { "Not_implemented": 13, "Broken": 2, "Completed": 1, "date": "2012-11-01T07:00:00.000Z" }, { "Not_implemented": 29, "Broken": 4, "Completed": 5, "date": "2012-12-01T08:00:00.000Z" }, { "Not_implemented": 30, "Broken": 4, "Completed": 5, "date": "2013-01-01T08:00:00.000Z" }, { "Not_implemented": 31, "Broken": 4, "Completed": 6, "date": "2013-02-01T08:00:00.000Z" }, { "Not_implemented": 32, "Broken": 4, "Completed": 7, "date": "2013-03-01T08:00:00.000Z" }, { "Not_implemented": 34, "Broken": 4, "Completed": 7, "date": "2013-04-01T07:00:00.000Z" }, { "Not_implemented": 37, "Broken": 4, "Completed": 7, "date": "2013-05-01T07:00:00.000Z" }, { "Not_implemented": 37, "Broken": 5, "Completed": 9, "date": "2013-06-01T07:00:00.000Z" }, { "Not_implemented": 47, "Broken": 6, "Completed": 9, "date": "2013-07-01T07:00:00.000Z" }, { "Not_implemented": 49, "Broken": 6, "Completed": 9, "date": "2013-08-01T07:00:00.000Z" }, { "Not_implemented": 51, "Broken": 6, "Completed": 9, "date": "2013-09-01T07:00:00.000Z" }, { "Not_implemented": 53, "Broken": 6, "Completed": 10, "date": "2013-10-01T07:00:00.000Z" }, { "Not_implemented": 54, "Broken": 6, "Completed": 10, "In_alpha": 1, "date": "2013-11-01T07:00:00.000Z" }, { "Not_implemented": 62, "Broken": 6, "Completed": 10, "In_alpha": 1, "date": "2013-12-01T08:00:00.000Z" }, { "Not_implemented": 67, "Broken": 7, "Completed": 16, "In_alpha": 2, "date": "2014-01-01T08:00:00.000Z" }, { "Not_implemented": 93, "Broken": 8, "Completed": 30, "In_alpha": 3, "Compromised": 1, "date": "2014-02-01T08:00:00.000Z" }, { "Not_implemented": 114, "Broken": 9, "Completed": 32, "In_alpha": 4, "Compromised": 1, "date": "2014-03-01T08:00:00.000Z" }, { "Not_implemented": 136, "Broken": 9, "Completed": 35, "In_alpha": 4, "Compromised": 1, "date": "2014-04-01T07:00:00.000Z" }, { "Not_implemented": 154, "Broken": 11, "Completed": 38, "In_alpha": 4, "Compromised": 1, "date": "2014-05-01T07:00:00.000Z" }, { "Not_implemented": 165, "Broken": 11, "Completed": 38, "In_alpha": 4, "Compromised": 1, "date": "2014-06-01T07:00:00.000Z" }, { "Not_implemented": 182, "Broken": 14, "Completed": 39, "In_alpha": 4, "Compromised": 1, "date": "2014-07-01T07:00:00.000Z" }, { "Not_implemented": 186, "Broken": 15, "Completed": 40, "In_alpha": 5, "Compromised": 1, "date": "2014-08-01T07:00:00.000Z" }, { "Not_implemented": 197, "Broken": 15, "Completed": 42, "In_alpha": 6, "Compromised": 1, "date": "2014-09-01T07:00:00.000Z" }, { "Not_implemented": 199, "Broken": 15, "Completed": 42, "In_alpha": 7, "Compromised": 1, "date": "2014-10-01T07:00:00.000Z" }, { "Not_implemented": 199, "Broken": 15, "Completed": 42, "In_alpha": 7, "Compromised": 1, "date": "2014-11-01T07:00:00.000Z" }, { "Not_implemented": 202, "Broken": 17, "Completed": 42, "In_alpha": 7, "Compromised": 1, "date": "2014-12-01T08:00:00.000Z" }, { "Not_implemented": 204, "Broken": 17, "Completed": 43, "In_alpha": 7, "Compromised": 1, "date": "2015-01-01T08:00:00.000Z" }, { "Not_implemented": 205, "Broken": 17, "Completed": 58, "In_alpha": 7, "Compromised": 1, "date": "2015-02-01T08:00:00.000Z" }, { "Not_implemented": 205, "Broken": 17, "Completed": 58, "In_alpha": 7, "Compromised": 1, "date": "2015-03-01T08:00:00.000Z" }, { "Not_implemented": 206, "Broken": 19, "Completed": 58, "In_alpha": 7, "Compromised": 1, "date": "2015-04-01T07:00:00.000Z" }, { "Not_implemented": 208, "Broken": 19, "Completed": 58, "In_alpha": 7, "Compromised": 1, "date": "2015-05-01T07:00:00.000Z" }, { "Not_implemented": 209, "Broken": 19, "Completed": 58, "In_alpha": 7, "Compromised": 1, "date": "2015-06-01T07:00:00.000Z" }, { "Not_implemented": 210, "Broken": 19, "Completed": 58, "In_alpha": 7, "Compromised": 1, "date": "2015-07-01T07:00:00.000Z" }, { "Not_implemented": 210, "Broken": 19, "Completed": 58, "In_alpha": 7, "Compromised": 1, "date": "2015-08-01T07:00:00.000Z" }, { "Not_implemented": 211, "Broken": 20, "Completed": 58, "In_alpha": 7, "Compromised": 1, "date": "2015-09-01T07:00:00.000Z" }, { "Not_implemented": 212, "Broken": 20, "Completed": 59, "In_alpha": 7, "Compromised": 1, "date": "2015-10-01T07:00:00.000Z" }, { "Not_implemented": 212, "Broken": 21, "Completed": 59, "In_alpha": 7, "Compromised": 1, "date": "2015-11-01T07:00:00.000Z" }, { "Not_implemented": 212, "Broken": 21, "Completed": 59, "In_alpha": 7, "Compromised": 1, "date": "2015-12-01T08:00:00.000Z" }, { "Not_implemented": 213, "Broken": 21, "Completed": 59, "In_alpha": 7, "Compromised": 1, "date": "2016-01-01T08:00:00.000Z" }, { "Not_implemented": 213, "Broken": 22, "Completed": 62, "In_alpha": 7, "Compromised": 1, "date": "2016-02-01T08:00:00.000Z" }, { "Not_implemented": 213, "Broken": 22, "Completed": 62, "In_alpha": 7, "Compromised": 1, "date": "2016-03-01T08:00:00.000Z" }, { "Not_implemented": 223, "Broken": 22, "Completed": 62, "In_alpha": 7, "Compromised": 1, "date": "2016-04-01T07:00:00.000Z" }, { "Not_implemented": 223, "Broken": 22, "Completed": 63, "In_alpha": 7, "Compromised": 1, "date": "2016-05-01T07:00:00.000Z" }, { "Not_implemented": 223, "Broken": 22, "Completed": 63, "In_alpha": 7, "Compromised": 1, "date": "2016-06-01T07:00:00.000Z" }, { "Not_implemented": 223, "Broken": 22, "Completed": 63, "In_alpha": 7, "Compromised": 1, "date": "2016-07-01T07:00:00.000Z" }, { "Not_implemented": 223, "Broken": 22, "Completed": 63, "In_alpha": 7, "Compromised": 1, "date": "2016-08-01T07:00:00.000Z" }, { "Not_implemented": 227, "Broken": 22, "Completed": 63, "In_alpha": 7, "Compromised": 1, "date": "2016-09-01T07:00:00.000Z" }, { "Not_implemented": 227, "Broken": 22, "Completed": 63, "In_alpha": 7, "Compromised": 1, "date": "2016-10-01T07:00:00.000Z" }, { "Stagnant": 4, "Broken": 22, "Completed": 63, "Not_implemented": 248, "In_alpha": 7, "Compromised": 1, "date": "2016-11-01T07:00:00.000Z" }, { "Stagnant": 24, "Broken": 23, "Completed": 63, "Not_implemented": 229, "In_alpha": 7, "Compromised": 1, "date": "2016-12-01T08:00:00.000Z" }, { "Stagnant": 29, "Broken": 23, "Completed": 63, "Not_implemented": 224, "In_alpha": 7, "Compromised": 1, "date": "2017-01-01T08:00:00.000Z" }, { "Stagnant": 31, "Broken": 23, "Completed": 63, "Not_implemented": 222, "In_alpha": 7, "Compromised": 1, "date": "2017-02-01T08:00:00.000Z" }, { "Stagnant": 31, "Broken": 23, "Completed": 63, "Not_implemented": 222, "In_alpha": 7, "Compromised": 1, "date": "2017-03-01T08:00:00.000Z" }, { "Stagnant": 32, "Broken": 23, "Completed": 63, "Not_implemented": 221, "In_alpha": 7, "Compromised": 1, "date": "2017-04-01T07:00:00.000Z" }, { "Stagnant": 35, "Broken": 23, "Completed": 63, "Not_implemented": 218, "In_alpha": 7, "Compromised": 1, "date": "2017-05-01T07:00:00.000Z" }
     ];
-    var brokenArray = [], stagnantArray = [], notimplementedArray = [], inalphaArray = [], compromisedArray = [], completedArray = [];
+    var brokenArray = [], stagnantArray = [], notimplementedArray = [], inalphaArray = [], compromisedArray = [], completedArray = [], labels = [];
     //build data arrays
-    var labels = [];
     for (var _i = 0, History_1 = History; _i < History_1.length; _i++) {
         var month = History_1[_i];
         switch (para_Type) {
@@ -194,39 +270,42 @@ function Build_and_fill_Chart(para_Type) {
                 borderColor: "#c56d6d",
                 borderWidth: 1,
                 data: brokenArray,
+                spanGaps: true,
             }, {
                 label: "Stagnant",
                 backgroundColor: "#fcddc4",
                 borderColor: "#f5903d",
-                borderWidth: 1,
                 data: stagnantArray
             }, {
                 label: "Not implemented",
                 backgroundColor: "#fcf8e3",
                 borderColor: "#ecd046",
-                borderWidth: 1,
                 data: notimplementedArray
             }, {
                 label: "In alpha",
                 backgroundColor: "#d9edf7",
                 borderColor: "#57afdb",
-                borderWidth: 1,
                 data: inalphaArray
             }, {
                 label: "Compromised",
                 backgroundColor: "#ccdde8",
                 borderColor: "#72a1c0",
-                borderWidth: 1,
                 data: compromisedArray
             }, {
                 label: "Completed",
                 backgroundColor: "#dff0d8",
                 borderColor: "#86c66c",
-                borderWidth: 1,
                 data: completedArray
             }]
     };
-    //console.log(typeof (AllChart));
+    //add any shared elements to all datasets
+    for (var i = 0; i < data.datasets.length; i++) {
+        data.datasets[i].borderWidth = 1;
+        data.datasets[i].pointRadius = 1;
+        data.datasets[i].pointHitRadius = 10;
+        data.datasets[i].pointHoverRadius = 6;
+        data.datasets[i].pointHoverBorderWidth = 3;
+    }
     //update chart data if already created
     if (typeof (AllChart) == "object") {
         AllChart.config.data = data;
@@ -240,11 +319,12 @@ function Build_and_fill_Chart(para_Type) {
             data: data,
             options: {
                 tooltips: {
-                    mode: "label"
+                    mode: "label",
+                    position: "nearest"
                 },
                 scales: {
                     xAxes: [{ stacked: false,
-                            ticks: { autoSkip: true },
+                            ticks: { autoSkip: false },
                             type: 'time',
                             time: {
                                 displayFormats: {
@@ -266,83 +346,5 @@ function Build_and_fill_Chart(para_Type) {
         });
     }
 }
-
 //Build AllChart with default input
-Build_and_fill_Chart(); 
-
-
-//scrolling to anchor
-(function(document, history, location) {
-  var HISTORY_SUPPORT = !!(history && history.pushState);
-
-  var anchorScrolls = {
-    ANCHOR_REGEX: /^#[^ ]+$/,
-    OFFSET_HEIGHT_PX: 50,
-
-    /**
-     * Establish events, and fix initial scroll position if a hash is provided.
-     */
-    init: function() {
-      this.scrollToCurrent();
-      $(window).on('hashchange', $.proxy(this, 'scrollToCurrent'));
-      $('body').on('click', 'a', $.proxy(this, 'delegateAnchors'));
-    },
-
-    /**
-     * Return the offset amount to deduct from the normal scroll position.
-     * Modify as appropriate to allow for dynamic calculations
-     */
-    getFixedOffset: function() {
-      return this.OFFSET_HEIGHT_PX;
-    },
-
-    /**
-     * If the provided href is an anchor which resolves to an element on the
-     * page, scroll to it.
-     * @param  {String} href
-     * @return {Boolean} - Was the href an anchor.
-     */
-    scrollIfAnchor: function(href, pushToHistory) {
-      var match, anchorOffset;
-
-      if(!this.ANCHOR_REGEX.test(href)) {
-        return false;
-      }
-
-      match = document.getElementById(href.slice(1));
-
-      if(match) {
-        anchorOffset = $(match).offset().top - this.getFixedOffset();
-        $('html, body').animate({ scrollTop: anchorOffset});
-
-        // Add the state to history as-per normal anchor links
-        if(HISTORY_SUPPORT && pushToHistory) {
-          history.pushState({}, document.title, location.pathname + href);
-        }
-      }
-
-      return !!match;
-    },
-    
-    /**
-     * Attempt to scroll to the current location's hash.
-     */
-    scrollToCurrent: function(e) { 
-      if(this.scrollIfAnchor(window.location.hash) && e) {
-      	e.preventDefault();
-      }
-    },
-
-    /**
-     * If the click event's target was an anchor, fix the scroll position.
-     */
-    delegateAnchors: function(e) {
-      var elem = e.target;
-
-      if(this.scrollIfAnchor(elem.getAttribute('href'), true)) {
-        e.preventDefault();
-      }
-    }
-  };
-$(document).ready($.proxy(anchorScrolls, 'init'));
-})(window.document, window.history, window.location);
+Build_and_fill_Chart();
