ac99a7f8ee0c2a7640bb8d981c777786736c5a08
StarCitizenTracker <StarCitizenTracker@users.noreply.github.com>
2018-02-26 07:02:46 -0800

    Add maintainers

--- js/main.js
+++ js/main.js
@@ -48,7 +48,7 @@
     //$('#confidence-btn').tooltip('show');
     
     // Select and replace maintained by name
-    var maintainers = ["CryTek Legal Beagles", "Coutts & Co.", "Bitter EvE Goons", "SomethingAwful Goons", "Salty Asshats", "Goons (who know nothing about game development)", "Goons stuck in checkmate", "T-posed Goons", "MY GIRLFRIEND", "The Fourth Stimpire", "WaffleImages", "COBOL Greybeards", "Blocked Buddies", "Ryan Archer"]
+    var maintainers = ["CryTek Legal Beagles", "Coutts & Co.", "Bitter EvE Goons", "SomethingAwful Goons", "Salty Asshats", "Goons (who know nothing about game development)", "Goons stuck in checkmate", "T-posed Goons", "MY GIRLFRIEND", "The Fourth Stimpire", "WaffleImages", "COBOL Greybeards", "Blocked Buddies", "Thanks notepad", "Archive-Priests™", "Ryan Archer"]
     var maintainer = maintainers[Math.floor(Math.random() * maintainers.length)];
     $('.maintainer-name').html(maintainer);
 
